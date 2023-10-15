5f18bcc46bafffa262a6a46a1faeeba84040857f
ziginfo <34913387+ziginfo@users.noreply.github.com>
2023-10-15 12:19:31 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# S Wing Control
+Control your LightShark LS-Wing with chataigne (OSC implementation)
+Select Mode 5 (pressing SEL-5 button) at startup of your LS-Wing to use the OSC/UDP-Mode
+
+The LS-Wing is a multitask hardware controller. It has 6 rows of 10 buttons and 10 faders, all of them configurable twice on two pages.
+Initially it controls the LightShark LS-1 and LS-Core (Modes 1 to 3); but it can also be configured as a pure MIDI-controller (Mode4) or OSC/UDP controller (Mode5)
+
+You can connect to its Web-Server via the default addresses lswing.local or 2.0.0.11 or by a freely configurable (static) IP address to integrate into your network
+The default OSC Ports are
+Incoming: 8000 and Outgoing: 9000 (but can be changed if necessary!)
+
+the LS-Wing can also be controlled by DMX-Remote and the LS-Wing integrate two Nodes (ArtNet or ACN versus DMX).
+All these features make the LS-Wing quite unique and versatile.  
---
+++ lswing.js
@@ -0,0 +1,72 @@
+
+
+var myParameters = {};
+/*
+function oscEvent(address, args) {
+	script.log(" Message received : " + address);
+}
+
+function update(deltaTime) {
+	var now = util.getTime();
+	if(now > TSSendAlive) {
+		TSSendAlive = now + 5;
+		keepAlive();
+	}
+}
+
+function keepAlive() {
+	local.send("/LSW/Sync",0.0);
+}
+
+*/
+
+
+/// Controls
+
+function push_button(page, button, val) {
+	local.send("/LSW/Button/"+page+"/"+button+"", val);
+}
+
+function toggle_button(page, button, val) {
+	local.send("/LSW/ToggleButton/"+page+"/"+button+"");
+}
+
+function focus_button(button, val) {
+	local.send("/LSW/FocusButton/"+button+"", val);
+}
+
+function fader(page, fader, val) {
+	val= Math.round (val) ;
+	local.send("/LSW/Fader/"+page+"/"+fader+"", val);
+}
+
+function focus_fader(fader, val) {
+	val= Math.round (val) ;
+	local.send("/LSW/FocusFader/"+fader+"", val);
+}
+
+/// Actions
+
+function page(page) {
+	local.send("/LSW/Page", page);
+}
+
+function sync() {
+	local.send("/LSW/Sync", 0.0);
+}
+
+function sync_faders() {
+	local.send("/LSW/Sync/Faders", 0.0);
+}
+
+function sync_buttons() {
+	local.send("/LSW/Sync/Buttons", 0.0);
+}
+
+function reset() {
+	local.send("/LSW/Buttons/Reset", 0.0);
+}
+
+function reboot() {
+	local.send("/LSW/Reboot", 0.0);
+}
