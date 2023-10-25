9886860a0bce54562d5d6769e4e177c26e283ef3
ziginfo <34913387+ziginfo@users.noreply.github.com>
2023-10-25 15:12:23 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# LS Wing Chataigne Control
-Control the LightShark LS-Wing with Chataigne by OSC
+## S Wing Control
+Control your LightShark LS-Wing with chataigne (OSC implementation)
+Select Mode 5 (pressing SEL-5 button) at startup of your LS-Wing to use the OSC/UDP-Mode
 
-Select Mode-5 (pressing SEL-5 button) at startup of your LS-Wing to use the OSC/UDP-Mode
-and "OSC Remote Control" must be set to "ON" in the "Wing Settings" !
+The LS-Wing is a multitask hardware controller. It has 6 rows of 10 buttons and 10 faders, all of them configurable twice on two pages.
+Initially it controls the LightShark LS-1 and LS-Core (Modes 1 to 3); but it can also be configured as a pure MIDI-controller (Mode4) or OSC/UDP controller (Mode5)
 
-The default OSC Ports are :
+You can connect to its Web-Server via the default addresses lswing.local or 2.0.0.11 or by a freely configurable (static) IP address to integrate into your network
+notice when connecting with the default address : "lswing.local", there will be no feedback sent back to Chataigne
+Feedback will be provided by regular IP address (as 2.0.0.11 ou 192.168.xx.xx etc)
+The default OSC Ports are
 Incoming: 8000 and Outgoing: 9000 (but can be changed if necessary!)
 
-The LS-Wing is a multitask controller. It has 6 rows of 10 buttons and 10 faders, all of them configurable on two pages.
+the LS-Wing can also be controlled by DMX-Remote and the LS-Wing integrate two Nodes (ArtNet or ACN versus DMX).
+All these features make the LS-Wing quite unique and versatile. 
 
-Initially it controls the LightShark LS-1 and LS-Core (Mode-1 to 3); but it can also be configured as a pure MIDI-controller (Mode-4) or OSC/UDP controller (Mode-5)
-
-You can connect to its Web-Server via the default addresses lswing.local or 2.0.0.11 or by a freely configurable (static) IP address to integrate it into your network
-
-The LS-Wing can also be controlled by DMX-Remote and the LS-Wing integrate two Nodes (ArtNet or ACN versus DMX).
-
-All these features make the LS-Wing quite unique and versatile.  
+### Version 1.2
+added containers for monitoring Fader and Button-Values 
--- lswing.js
+++ lswing.js
@@ -1,15 +1,140 @@
 
 
 var myParameters = {};
-/*
+
+function init() {
+
+		
+//Channel Fader  Container
+	
+	faders = local.values.addContainer("Faders Page1");
+	faders.setCollapsed(true);
+		for (var i = 1; i<=10; i++) {
+		faders.addFloatParameter("Fader "+(i), "", 0, 0, 255);  }	
+	faders = local.values.addContainer("Faders Page2");
+	faders.setCollapsed(true);
+		for (var i = 1; i<=10; i++) {
+		faders.addFloatParameter("Fader "+(i), "", 0, 0, 255);  }
+		
+//Buttons Containers Page1
+	buttcont = local.values.addContainer("Buttons Page One");
+	buttcont.setCollapsed(true);
+	buttcont = local.values.addContainer("Buttons Page Two");
+	buttcont.setCollapsed(true);
+		
+	buttons = local.values.buttonsPageOne.addContainer("Buttons Row1");
+	buttons.setCollapsed(true);
+		for (var i = 1; i<=10; i++) {
+		buttons.addBoolParameter("Button "+(i), "","");  }
+		
+	buttons = local.values.buttonsPageOne.addContainer("Buttons Row2");
+	buttons.setCollapsed(true);
+		for (var i = 11; i<=20; i++) {
+		buttons.addBoolParameter("Button "+(i), "","");  }
+		
+	buttons = local.values.buttonsPageOne.addContainer("Buttons Row3");
+	buttons.setCollapsed(true);
+		for (var i = 21; i<=30; i++) {
+		buttons.addBoolParameter("Button "+(i), "","");  }
+		
+	buttons = local.values.buttonsPageOne.addContainer("Buttons Row4");
+	buttons.setCollapsed(true);
+		for (var i = 31; i<=40; i++) {
+		buttons.addBoolParameter("Button "+(i), "","");  }
+		
+	buttons = local.values.buttonsPageOne.addContainer("Buttons Row5");
+	buttons.setCollapsed(true);
+		for (var i = 41; i<=50; i++) {
+		buttons.addBoolParameter("Button "+(i), "","");  }
+		
+	buttons = local.values.buttonsPageOne.addContainer("Buttons Row6");
+	buttons.setCollapsed(true);
+		for (var i = 51; i<=60; i++) {
+		buttons.addBoolParameter("Button "+(i), "","");  }
+		
+//Buttons Containers Page2
+	
+		
+	buttons = local.values.buttonsPageTwo.addContainer("Buttons Row1");
+	buttons.setCollapsed(true);
+		for (var i = 1; i<=10; i++) {
+		buttons.addBoolParameter("Button "+(i), "","");  }
+		
+	buttons = local.values.buttonsPageTwo.addContainer("Buttons Row2");
+	buttons.setCollapsed(true);
+		for (var i = 11; i<=20; i++) {
+		buttons.addBoolParameter("Button "+(i), "","");  }
+		
+	buttons = local.values.buttonsPageTwo.addContainer("Buttons Row3");
+	buttons.setCollapsed(true);
+		for (var i = 21; i<=30; i++) {
+		buttons.addBoolParameter("Button "+(i), "","");  }
+		
+	buttons = local.values.buttonsPageTwo.addContainer("Buttons Row4");
+	buttons.setCollapsed(true);
+		for (var i = 31; i<=40; i++) {
+		buttons.addBoolParameter("Button "+(i), "","");  }
+		
+	buttons = local.values.buttonsPageTwo.addContainer("Buttons Row5");
+	buttons.setCollapsed(true);
+		for (var i = 41; i<=50; i++) {
+		buttons.addBoolParameter("Button "+(i), "","");  }
+		
+	buttons = local.values.buttonsPageTwo.addContainer("Buttons Row6");
+	buttons.setCollapsed(true);
+		for (var i = 51; i<=60; i++) {
+		buttons.addBoolParameter("Button "+(i), "","");  }
+		
+}		
+
 function oscEvent(address, args) {
 	script.log(" Message received : " + address);
+		
+			
+	// Faders Page 1
+		for (var n = 1; n<= 10; n++) {
+		if (address == "/LSW/Fader/1/"+n){
+			local.values.fadersPage1.getChild('fader'+n).set(args[0]);  } }	
+	// Faders Page 2
+		for (var n = 1; n<= 10; n++) {
+		if (address == "/LSW/Fader/2/"+n){
+			local.values.fadersPage2.getChild('fader'+n).set(args[0]);  } }	
+			
+			
+	// Buttons Page1 Row1
+		for (var n = 1; n<= 10; n++) {
+		if (address == "/LSW/Button/1/"+n){
+			local.values.buttonsPageOne.buttonsRow1.getChild('button'+n).set(args[0]);  } }
+	// Buttons Page1 Row2
+		for (var n = 11; n<= 20; n++) {
+		if (address == "/LSW/Button/1/"+n){
+			local.values.buttonsPageOne.buttonsRow2.getChild('button'+n).set(args[0]);  } }
+	// Buttons Page1 Row3
+		for (var n = 21; n<= 30; n++) {
+		var b= n+30 ;
+		if (address == "/LSW/Button/1/"+b){
+			local.values.buttonsPageOne.buttonsRow3.getChild('button'+n).set(args[0]);  } }
+	// Buttons Page1 Row4
+		for (var n = 31; n<= 40; n++) {
+		if (address == "/LSW/Button/1/"+n){
+			local.values.buttonsPageOne.buttonsRow4.getChild('button'+n).set(args[0]);  } }
+	// Buttons Page1 Row5
+		for (var n = 41; n<= 50; n++) {
+		if (address == "/LSW/Button/1/"+n){
+			local.values.buttonsPageOne.buttonsRow5.getChild('button'+n).set(args[0]);  } }
+	// Buttons Page1 Row6
+		for (var n = 51; n<= 60; n++) {
+		var b= n-30 ;
+		if (address == "/LSW/Button/1/"+b){
+			local.values.buttonsPageOne.buttonsRow6.getChild('button'+n).set(args[0]);  } }
+		
 }
+
 
 function update(deltaTime) {
 	var now = util.getTime();
 	if(now > TSSendAlive) {
-		TSSendAlive = now + 5;
+		TSSendAlive = now + 1;
 		keepAlive();
 	}
 }
@@ -18,7 +143,7 @@ function keepAlive() {
 	local.send("/LSW/Sync",0.0);
 }
 
-*/
+
 
 
 /// Controls
